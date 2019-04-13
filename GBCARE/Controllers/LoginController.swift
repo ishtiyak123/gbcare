861ef9422cd24c4895acd1d60398ed5514b70a56
ishtiyak395 <ishtiyak395@gmail.com>
2019-04-13 12:50:31 +0530

    update login vc by usual

--- GBCARE/Controllers/LoginController.swift
+++ GBCARE/Controllers/LoginController.swift
@@ -12,6 +12,7 @@ class LoginController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ugfiygf")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
