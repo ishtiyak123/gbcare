f1dd07243ce1af99ad15a2cff1c6226e0910511b
MD ISHTIYAK <mdishtiyak@MDs-MacBook-Air.local>
2019-04-13 12:40:54 +0530

    commit by anirban

--- GBCARE/Controllers/LoginController.swift
+++ GBCARE/Controllers/LoginController.swift
@@ -12,6 +12,8 @@ class LoginController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(".kdsfjhg")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
